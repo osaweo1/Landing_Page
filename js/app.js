1fe5b1191d7628b8061d6cf2eb39f63b64e2cc5a
osaweo1 <osamudiamenosawe@gmail.com>
2022-04-26 14:41:12 +0100

    update js and html to scrow page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * declaration of global variables
  * and getting element on our HTML page
  */
+
 let sections = Array.from(document.querySelectorAll('section'));
 
 const navList = document.querySelector('#navbar__list');
@@ -15,43 +16,76 @@ function createAnchor() {
         //dynamically creating an anchor
         const anchor = document.createElement('a');
         //adding a class Menu__link to created Anchor
-        anchor.classList.add('menu__link');
+        anchor.setAttribute('class',
+            'menu__link ');
         //getting the attribute of ID from each section
         let link = sections[a].getAttribute('id');
         //giving the section ID attribute to the Anchor
-        anchor.href = '#' + link;
-        //geeting the text value
+
+        //getting the text value and setting it to the anchor
         anchor.innerText = sections[a].getAttribute('data-nav');
         // creating a list element
         let list = document.createElement('li');
+        //sect class attribute for the List items
+        list.setAttribute('class', link);
+        //sect data_link attribute for the List items
+        list.setAttribute('data_link', link);
         //appending the Anchor as a child to the created list
         list.appendChild(anchor);
         // An event listener for window scrolling
-        window.addEventListener('scroll', function() {
-            onScroll(anchor);
+        document.addEventListener('scroll', function() {
+            onScroll();
 
-        })
+
+        });
 
         // Appending the list as the ul child
-        navList.appendChild(list)
+        navList.appendChild(list);
+
     }
 
 
+    //function to get the view port and to add your-active-class  to the section visible in the viewport and a menu_active link to active link
+    let lists = Array.from(document.querySelectorAll('li'))
+    console.log(lists);
+    let onScroll = function(anchor) {
+
+        let active = "";
+        //for Each method to check f asection is in viewport
+        sections.forEach(section => {
+
+            let rect = section.getBoundingClientRect();
+
+            if (rect.top >= -20 && rect.left >= 0 && rect.right <= window.innerWidth && rect.bottom <= window.innerHeight) {
+                //setting an active class to the section visible in the view port
+                section.classList.add('your-active-class');
+                active = section.getAttribute('id')
+
+
+
+            } else { section.classList.remove('your-active-class') }
+
+        });
+        //setting a menu_active link to a active link.
+        lists.forEach(list => {
+            list.classList.remove('menu__active');
+            if (list.classList.contains(active)) {
+                list.classList.add('menu__active');
+            }
+            // an eventlistener on the list item to Scroll to section
+            list.addEventListener('click', () => {
+                const scr = document.getElementById(list.getAttribute('data_link'));
+
+                scr.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            });
+
+
+        });
+
+
+    }
+
 
 }
-
-// running the function
 createAnchor();
-
-//function to get the view port and to add your-active-class  to the section visible in the viewport
-let onScroll = function(anchor) {
-    sections.forEach(section => {
-        let rect = section.getBoundingClientRect();
-        console.log(rect);
-        if (rect.top >= -17 && rect.left >= 0 && rect.right <= window.innerWidth && rect.bottom <= window.innerHeight) {
-            section.classList.add('your-active-class');
-
-        } else { section.classList.remove('your-active-class') }
-
-    })
-}
+// running the function*/
